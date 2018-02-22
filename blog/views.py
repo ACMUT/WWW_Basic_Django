43f1344fe807301db57f70f60389dd99f05a546f
borna <bornaarzi@gmail.com>
2018-02-22 17:57:24 +0330

    add get parameter for filtering author

--- blog/views.py
+++ blog/views.py
@@ -4,5 +4,9 @@ from blog.models import BlogPost
 
 
 def get_posts(request):
-    context = {'posts': BlogPost.objects.all()}
+    author = request.GET.get('author')
+    if author:
+        context = {'posts': BlogPost.objects.filter(author=author)}
+    else:
+        context = {'posts': BlogPost.objects.all()}
     return render(request, 'blog/index.html', context)
