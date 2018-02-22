046bd92bb009df5e8ee5bac50fda0d29f952b399
borna <bornaarzi@gmail.com>
2018-02-22 16:12:58 +0330

    add modelAdmin to blog admin page

--- blog/admin.py
+++ blog/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 
 from blog.models import Blog
 
-admin.site.register(Blog)
+
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_at')
+    list_filter = ('author',)
+
+
+admin.site.register(Blog, BlogAdmin)
