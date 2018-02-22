f173632d1bcc08be9d64eeb83ec9143a4fd0e8cb
borna <bornaarzi@gmail.com>
2018-02-22 16:17:27 +0330

    change model name to BlogPost

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 
-from blog.models import Blog
+from blog.models import BlogPost
 
 
-class BlogAdmin(admin.ModelAdmin):
+class BlogPostAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'created_at')
     list_filter = ('author',)
 
 
-admin.site.register(Blog, BlogAdmin)
+admin.site.register(BlogPost, BlogPostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class Blog(models.Model):
+class BlogPost(models.Model):
     author = models.CharField(max_length=255, null=False)
     title = models.CharField(max_length=255, null=False, unique=True)
     content = models.TextField(max_length=4000)
